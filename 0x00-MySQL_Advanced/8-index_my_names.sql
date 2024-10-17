c39faef41d4a597d04708ab7b3a2a7e09f8c9b6e
Nathan Machoka <natemachoka@gmail.com>
2024-10-17 12:12:08 -0700

    8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,2 @@
--- Drop the index if it already exists to avoid errors
-DROP INDEX IF EXISTS idx_name_first ON names;
-
 -- Create an index on the first letter of the name
 CREATE INDEX idx_name_first ON names (name(1));
